3c263b204006cd5964964db9a82a14f2980334ca
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-01 22:06:14 +0900

    Container and Space

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_01_101444) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_01_124450) do
   create_table "colors", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "containers", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
