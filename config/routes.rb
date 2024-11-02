22d0d420d846c455f45fa2e119c6be90179e3434
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-02 18:30:20 +0900

    Layout and Position

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :positions
   resources :sizings
   resources :typographies
   resources :containers
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_01_135847) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_085633) do
   create_table "colors", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_01_135847) do
   end
 
   create_table "containers", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
